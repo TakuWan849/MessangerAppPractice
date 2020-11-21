51b4ea79fd8a52b2a3fb62b2e9945c1f320ad2fe
TakuWan849 <wantaku339@gmail.com>
2020-11-21 21:01:59 +0900

    ログイン画面を作成しました

--- MessangerApp/Controllers/ConversationsViewController.swift
+++ MessangerApp/Controllers/ConversationsViewController.swift
@@ -12,18 +12,28 @@ class ConversationsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    // MARK:- v iewDidAppear
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let isLoggedIn = UserDefaults.standard.bool(forKey: "logged_in")
+        
+        // isLoggedInがfalseならログイン画面に遷移
+        if !isLoggedIn {
+            let vc = LoginViewController()
+            let navVC = UINavigationController(rootViewController: vc)
+            navVC.modalPresentationStyle = .fullScreen
+            
+            present(navVC, animated: true)
+        }
+        
     }
-    */
+
 
 }
---
+++ MessangerApp/Controllers/Login/RegisterViewController.swift
@@ -0,0 +1,215 @@
+//
+//  RegisterViewController.swift
+//  MessangerApp
+//
+//  Created by 野澤拓己 on 2020/11/21.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    private let scrollView: UIScrollView = {
+       let view = UIScrollView()
+        view.clipsToBounds = true
+        return view
+    }()
+    
+    private let imageView : UIImageView = {
+       let imageView = UIImageView()
+        imageView.image = UIImage(named: "chat")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private let emailField: UITextField = {
+       let field = UITextField()
+        
+        field.backgroundColor = .white
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.returnKeyType = .continue
+        
+        field.layer.cornerRadius = 52/2
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        field.layer.masksToBounds = true
+        
+        field.leftViewMode = .always
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: 0))
+        
+        field.placeholder = "email address...."
+        return field
+        
+    }()
+    
+    private let passWordField: UITextField = {
+       let field = UITextField()
+        
+        field.backgroundColor = .white
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.returnKeyType = .done
+        field.isSecureTextEntry = true
+        
+        field.layer.cornerRadius = 52/2
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        field.layer.masksToBounds = true
+        
+        field.leftViewMode = .always
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: 0))
+        
+        field.placeholder = "passWord...."
+        return field
+        
+    }()
+    
+    private let LoginButton : UIButton = {
+        let button = UIButton()
+        button.setTitle("Log in", for: .normal)
+        button.backgroundColor = .link
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 12
+        button.layer.masksToBounds = true
+        button.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
+        
+        return button
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Log In"
+        view.backgroundColor = .white
+        
+        addSubViews()
+        configure()
+        
+    }
+    
+    // MARK: - addSubViews
+    private func addSubViews() {
+        
+        view.addSubview(scrollView)
+        scrollView.addSubview(emailField)
+        scrollView.addSubview(imageView)
+        scrollView.addSubview(passWordField)
+        scrollView.addSubview(LoginButton)
+    }
+    
+    // MARK: - viewDidLayoutSubViews
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        scrollView.frame = view.bounds
+        let size = scrollView.width / 3
+        
+        // logo
+        imageView.frame = CGRect(
+            x: (scrollView.width - size)/2,
+            y: 20,
+            width: size,
+            height: size
+        )
+        
+        // input email
+        emailField.frame = CGRect(
+            x: 30,
+            y: imageView.bottom + 30,
+            width: scrollView.width - 60,
+            height: 52
+        )
+        
+        // input password
+        passWordField.frame = CGRect(
+            x: 30,
+            y: emailField.bottom + 15,
+            width: scrollView.width - 60,
+            height: 52
+        )
+        
+        // tappedLogin
+        LoginButton.frame = CGRect(
+            x: 30,
+            y: passWordField.bottom + 30,
+            width: scrollView.width - 60,
+            height: 52
+        )
+    }
+    
+    // MARK:- configure
+    private func configure() {
+        
+        // NavigationBar
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: "Register",
+            style: .done,
+            target: self,
+            action: #selector(didTapRegister)
+        )
+        
+        // LoginButton
+        LoginButton.addTarget(self, action: #selector(didTapLoginButton), for: .touchUpInside)
+        
+        // emailField
+        emailField.delegate = self
+        
+        // passWordField
+        passWordField.delegate = self
+    }
+    
+    // MARK: - Actions
+    @objc private func didTapRegister() {
+        let vc = RegisterViewController()
+        vc.title = "Create Account"
+        navigationController?.pushViewController(vc, animated: true)
+        
+    }
+    
+    @objc private func didTapLoginButton() {
+        
+        // emailField, passWordFieldアンラップする
+        guard let email = emailField.text, let password = passWordField.text, !email.isEmpty, !password.isEmpty, password.count >= 6 else {
+            alertUserLoginError()
+            return
+        }
+        
+        // Firebase Log In
+        
+        
+    }
+    
+    func alertUserLoginError() {
+        let alert = UIAlertController(
+            title: "Woops",
+            message: "Please enter all information",
+            preferredStyle: .alert
+        )
+        
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+        
+        present(alert, animated: true)
+        
+    }
+    
+
+}
+
+extension RegisterViewController : UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        if textField == emailField {
+            passWordField.becomeFirstResponder()
+            
+        }
+        else if textField == passWordField {
+            didTapLoginButton()
+            
+        }
+        
+        return true
+    }
+
+}
