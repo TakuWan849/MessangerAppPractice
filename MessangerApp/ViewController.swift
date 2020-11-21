10035a8b3425befb077dda7126cdd88a4c388559
TakuWan849 <wantaku339@gmail.com>
2020-11-21 10:16:07 +0900

    Initial Commit

---
+++ MessangerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MessangerApp
+//
+//  Created by 野澤拓己 on 2020/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
