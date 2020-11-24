6a8e4cb6d4bad43fa9a4cab1d27f377c5d1e35a7
TakuWan849 <wantaku339@gmail.com>
2020-11-24 16:46:45 +0900

    ConversationsControllerの構成の作成を行いました。

--- MessangerApp/Controllers/ConversationsViewController.swift
+++ MessangerApp/Controllers/ConversationsViewController.swift
@@ -9,27 +9,61 @@ import UIKit
 import FirebaseAuth
 
 class ConversationsViewController: UIViewController {
-
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        
+        table.isHidden = true
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        return table
+    }()
+    
+    private let noConversationLabel : UILabel =  {
+        let label = UILabel()
+        label.text = "No Conversations!"
+        label.textAlignment = .center
+        label.textColor = .gray
+        label.font = .systemFont(ofSize: 21, weight: .medium)
+        label.isHidden = true
+        
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        DatabaseManager.shared.test()
+        configure()
+    }
+    
+    private func configure() {
+        
+        // tableView
+        view.addSubview(tableView)
+        setUpTableView()
+        
+        // label
+        view.addSubview(noConversationLabel)
+        fetchConversations()
+    }
+    
+    // MARK:- SetUP
+    private func setUpTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    private func fetchConversations() {
         
     }
     
-
-    
-    // MARK:- v iewDidAppear
+    // MARK:- viewDidAppear
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
         validateAuth()
-        
     }
     
-    
     private func validateAuth() {
-        
         // isLoggedInがfalseならログイン画面に遷移
         if FirebaseAuth.Auth.auth().currentUser == nil {
             let vc = LoginViewController()
@@ -41,7 +75,22 @@ class ConversationsViewController: UIViewController {
         
     }
     
+
+}
+
+extension ConversationsViewController: UITableViewDelegate, UITableViewDataSource {
     
-
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Hello world"
+        
+        return cell
+    }
+    
+    
+    
 }
